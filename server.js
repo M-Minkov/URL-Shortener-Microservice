51073cf514c9c5b2cdc8eafc4e61e743f7a74481
M-Minkov <MinkoMinkov@outlook.co.nz>
2021-08-23 23:56:42 +1200

    URL generation, and async/await
    implement unique short URL generation. As well as proper async await function usage. (instead of using promises or callbacks this time)
    
    Due to possible callback hell, and promises having limited scope

--- server.js
+++ server.js
@@ -2,25 +2,108 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose")
+
+const { Schema } = mongoose;
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-express.urlencoded({extended: true});
-
+// required middle-ware for body parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(cors());
 
+// allows public folder as URL for accessing stlye.css from main index.html file
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+// returns static index.html file when accessing MAIN-URL/
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
+
+// Default test
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+// URL Shortener Schema and Model (for saving URLs into the database)
+
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: Number
+})
+
+let urlModel = mongoose.model('urlModel', urlSchema);
+
+
+// Function checks if URL already exists, if not, checks how many are saved and autoincrements short_url for unique url
+async function createAndSaveUrl(url_to_shorten) {
+  try {
+    // finds if url exists
+    let urlDoc = await urlModel.findOne({original_url: url_to_shorten});
+
+    // if it doesn't, it's null, creates unique id, and saves to database
+    if(urlDoc == null) {
+      let short_id = await urlModel.countDocuments();
+      short_id += 1;
+      const newUrl = new urlModel({
+        original_url: url_to_shorten,
+        short_url: short_id
+      })
+      await newUrl.save();
+      // return id
+      return short_id;
+    }
+    // already exists, return gotten id
+    else {
+      return urlDoc.short_url;
+    }
+  }
+  // error catching
+  catch {
+    console.log("database is not working");
+    return null;
+  }
+};
+
+
+async function retrieve_url(req, res) {
+  let url_to_shorten = req.body.url;
+  let shortened = await createAndSaveUrl(url_to_shorten);
+  console.log(shortened);
+  res.json({"original_url": url_to_shorten, "short_url":shortened});
+}
+
+
+async function redirect(req, res) {
+  let short = req.params.short_url;
+  let urlDoc = await urlModel.findOne({short_url: short});
+
+  if(urlDoc == null) {
+    res.json({"error":"error"});
+  }
+  else {
+    const original_url = urlDoc.original_url;
+    res.redirect(original_url);
+  }
+}
+
+
+
+app.post("/api/shorturl", retrieve_url);
+
+app.get("/api/shorturl/:short_url", redirect);
+
+
+
+// Start Server
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
